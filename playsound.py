21c1e071788f03c9a573f4a6232e6c1e3110f3fb
balbonis <leyritana.jun.se@icloud.com>
2025-01-17 00:38:49 +0800

    mod3

--- playsound.py
+++ playsound.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 import winsound 
-message = "Now Playing"
+message = "Now Playing ...."
 @app.route("/")
 def home():
     winsound.PlaySound("sample.wav",winsound.SND_ASYNC)
